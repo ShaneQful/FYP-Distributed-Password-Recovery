1c4d432de67d0c5c57b01cc12d1539e01891ba0c
ShaneQful <shane_quigley@hotmail.com>
2013-01-18 18:28:41 +0000

    File Uplaod now works

--- sinatra_server.rb
+++ sinatra_server.rb
@@ -1,16 +1,24 @@
+require	'rubygems'
 require 'sinatra'
 
 set :public_folder, File.dirname(__FILE__) + '/'
+# set :files,  File.join(settings.public, 'files')
 
 get '/' do
-  "Hello World #{params[:name]}".strip
+	redirect '/UploadDialog.html'
 end
 
 post '/CrackingInProgress.html' do
-  out ="/CrackingInProgress.html?"
-  params.each do |k, v|
-    out += "#{k}=#{v}&"
-  end
-  file = File.open("CrackingInProgress.html", "rb")
-  out = file.read
+	unless params[:tocrack] &&
+					(tmpfile = params[:tocrack][:tempfile]) &&
+					(name = params[:tocrack][:filename])
+		@error = "No file selected"
+		redirect '/'
+	end
+	File.open(name, "w") { |f| f.write(tmpfile.read) }
+	params.each do |k, v|
+		out += "#{k}=#{v}&"
+	end
+	file = File.open("CrackingInProgress.html", "rb")
+	out = file.read
 end

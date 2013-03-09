50ed5075daef86cf7e5f6e003283e0d49f1c9cb3
ShaneQful <shane_quigley@hotmail.com>
2013-03-09 23:20:24 +0000

    More test cases

--- tc_sinatra_server.rb
+++ tc_sinatra_server.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 require_relative 'sinatra_server.rb'
 require 'test/unit'
 require 'rack/test'
@@ -5,19 +8,26 @@ require 'rack/test'
 ENV['RACK_ENV'] = 'test'
 
 class ServerTest < Test::Unit::TestCase
-  include Rack::Test::Methods
+	include Rack::Test::Methods
 
-  def app
-    Sinatra::Application
-  end
+	def app
+		Sinatra::Application
+	end
 
-  def test_it_serves_upload_dialog
-    get '/'
-    assert last_response.should be_redirect, 'No redirecting'
-  end
+	def test_it_serves_upload_dialog
+		get '/'
+		follow_redirect!
+		assert last_response.body.include?('src="upload.js"'), 'Upload dialog not serving'
+	end
 
-  def test_cracking_dialog_redirect
-    get '/CrackingInProgress.html'
-    assert last_response.should be_redirect, 'No redirecting'
-  end
+	def test_json_request
+		get '/done.json'
+		assert last_response.content_type.include?('json'), 'Not JSON Reponse'
+	end
+# 	def test_it_serves_cracking_dialog
+# 		post '/CrackingInProgress.html'
+# 		follow_redirect!
+# 		puts last_response.ok?
+# 		assert last_response.body.include?('src="upload.js"'), 'Cracking dialog not serving'
+# 	end
 end
